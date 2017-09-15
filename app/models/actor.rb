afc8c3d1d94c0c93da1054011e239ec2dcf57d97
deebnntt <daniellebennettchan@gmail.com>
2017-09-14 20:52:02 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
+
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    Character.all.select do |character|
+      character.actor == self
+    end.map do |character_info|
+      "#{character_info.name} - #{character_info.show.name}"
+    end
+  end
+
+
+
 end
